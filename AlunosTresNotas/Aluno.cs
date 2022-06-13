d183e477d20b1b9f562a23046dfca79bf7461267
ALUNO - CT SISTEMA <ALUNO - CT SISTEMA@DESKTOP-SD7BABS>
2022-06-13 07:51:28 -0300

    Adicionar arquivos de projeto.

---
+++ AlunosTresNotas/Aluno.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunosTresNotas
+{
+    
+
+    //classe que declara as variaveis
+        internal class Alunos
+        {
+            public string NomeAluno { get; set; }
+            public double Nota1 { get; set; }
+            public double Nota2 { get; set; }
+            public double Nota3 { get; set; }
+
+            public Alunos(string nomeAluno, double nota1, double nota2, double nota3)
+            {
+                NomeAluno = nomeAluno;
+                Nota1 = nota1;
+                Nota2 = nota2;
+                Nota3 = nota3;
+            }
+
+            // método para somar as notas
+            public double SomaNotas()
+            {
+                return Nota1 + Nota2 + Nota3;
+            }
+
+            // metodo para ver se o aluno foi reprovado ou aprovado de acordo com a nota
+            public string ReprovadoAprovado()
+            {
+                // se a soma da nota for maior ou igual a 60 
+                if (SomaNotas() >= 60)
+                {
+                    // mostra essa mensagem
+                    return "Parabéns voce foi APROVADO, sua nota final foi " + SomaNotas();
+                } 
+                // se a soma da nota for menor do que 60
+                else
+                {  
+                    // mostra essa mensagem
+                    return "Aluno REPROVADO, sua nota final foi " + SomaNotas() + " faltaram " + (60 - SomaNotas()) + " pontos para ser aprovado!";
+                }
+            }
+            // impressão padrão
+            public override string ToString()
+            {
+                return "***************DADOS DO ALUNO*****************\n"
+                + "Aluno(a) "
+                + NomeAluno.ToUpper() + "\n"
+                + "\n Nota 1 "
+                + Nota1.ToString("F2")
+                + "\n Nota 2 "
+                + Nota2.ToString("F2")
+                + "\n Nota 3 "
+                + Nota3.ToString("F2") + "\n\n"
+                + "***************APROVADO OU REPROVADO*****************\n"
+                + ReprovadoAprovado()
+                + "\n\n";
+            }
+        }
+}
---
+++ AlunosTresNotas/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace AlunosTresNotas
+{
+
+    class AlunosTresNotas
+    {
+
+        static void Main(string[] args)
+        {
+            // aqui é para você informar de quantos alunos você quer saber as notas
+            Console.WriteLine("As notas de quantos alunos voce quer ? ");
+            int qtdAlunos = int.Parse(Console.ReadLine());
+
+            //armazena o tamanho do vetor em variavel
+            Alunos[] alunos = new Alunos[qtdAlunos];
+
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+
+                Console.WriteLine(" ************DADOS DO " + (i + 1) + "° ALUNO ***********");
+                Console.WriteLine("Digite o nome do aluno : ");//pede para você digitar o nome do aluno
+                string nome = Console.ReadLine();//guarda o nome do aluno
+
+                Console.WriteLine("Digite a nota do 1º trimestre : (limite maximo da nota é 30)");//pede para digitar a nota do 1º trimestre
+                double nota1 = double.Parse(Console.ReadLine());//guarda a nota do 1º trimestre
+
+                while (nota1 > 30)
+                {
+
+                    if (nota1 > 30)
+                    {
+                        Console.WriteLine("Nota inválida, nota máxima é 30, digite novamente! ");
+                        Console.WriteLine("Digite a nota do 1º trimestre : (limite maximo da nota é 30)");
+                        nota1 = double.Parse(Console.ReadLine());
+                    }
+                }
+
+                Console.WriteLine("Digite a nota do 2º trimestre : (limite maximo da nota é 35)");//pede para digitar a nota do 2º trimestre
+                double nota2 = double.Parse(Console.ReadLine());//guarda a nota do 2º trimestre
+
+                while (nota2 > 35)
+                {
+
+                    if (nota2 > 35)
+                    {
+                        Console.WriteLine("Nota inválida, nota máxima é 35, digite novamente! ");
+                        Console.WriteLine("Digite a nota do 2º trimestre : (limite maximo da nota é 35)");
+                        nota2 = double.Parse(Console.ReadLine());
+                    }
+                }
+
+                Console.WriteLine("Digite a nota do 3º trimestre : (limite maximo da nota é 35)");//pede para digitar a nota do 3º trimestre
+                double nota3 = double.Parse(Console.ReadLine());//guarda a nota do 3º trimestre
+
+                while (nota3 > 35)
+                {
+                   
+                    if (nota3 > 35)
+                    {
+                        Console.WriteLine("Nota inválida, nota máxima é 35, digite novamente! ");
+                        Console.WriteLine("Digite a nota do 3º trimestre : (limite maximo da nota é 35)");
+                        nota3 = double.Parse(Console.ReadLine());
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                alunos[i] = new Alunos(nome, nota1, nota2, nota3);
+            }
+
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(" ************NOTA FINAL DO " + (i + 1) + "° ALUNO ***********");//mostra a nota  final do aluno
+                Console.WriteLine();
+
+                Console.WriteLine();
+                Console.WriteLine(alunos[i]);
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
