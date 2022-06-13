977e5f34d9a8067056277882690b1c39713c8f81
marissa2309 <102029727+marissa2309@users.noreply.github.com>
2022-06-13 08:12:55 -0300

    Add files via upload

---
+++ Retangulo1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados_retangulo
+{
+    class Retangulo1
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public Retangulo1(double largura, double altura)
+        {
+            Largura = largura;
+            Altura = altura;
+        }
+
+        public double area()
+        {
+            return Largura * Altura;
+        }
+
+        public double perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double diagonal()
+        {
+            return (double)Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+        //impressão padrão
+        public override string ToString()
+        {
+            return "\n AREA: "
+                + area()
+                + "\n PERIMETRO: "
+                + perimetro()
+                + "\n DIAGONAL: "
+                + diagonal();
+
+        }
+    }
+}
