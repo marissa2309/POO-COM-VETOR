a89253cae1b4a76443ce7f17fdf40cea891fa419
marissa2309 <102029727+marissa2309@users.noreply.github.com>
2022-06-13 07:51:42 -0300

    Add files via upload

---
+++ QuartoAluguel/QuartoAluguel/Cliente.cs
@@ -0,0 +1,125 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace QuartoAluguel
+//{
+//    class Cliente
+//    {
+//        public string Nome, DataNasc, email,Endereco;
+//        public string CPF;
+//        public string Telefone;
+//        public float Periodo;
+//        public double ValorQuarto = 800;
+//        public string quarto1,quarto2,quarto3;
+
+
+
+//        public double ValorTotal()
+//        {
+//            return Periodo * ValorQuarto;   
+
+//        }
+//        public override string ToString()
+//        {
+//            return "O QUARTO FOI ALUGADO POR:  "
+//                + Nome.ToUpper()
+//                + "\n * CPF: " + CPF.ToUpper()
+//                + "\n * TELEFONE: "
+//                + Telefone.ToUpper()
+//                + "\n * ENDEREÇO: "
+//                + Endereco.ToUpper()
+//                + "\n * DATA DE NASCIMENTO: "
+//                + DataNasc.ToUpper()
+//                + "\n * EMAIL: "
+//                + email.ToUpper()
+//                + "\n * VALOR MENSAL: "
+//                + ValorQuarto.ToString("C")
+//                + "\n * VALOR A PAGAR: "
+//                + ValorTotal().ToString("C");
+//        }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartoAluguel
+{
+    // classe onde é declarada as variaveis
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string DataNasc { get; set; }
+        public string Email { get; set; }
+
+
+
+
+        public Cliente(string nome, string cpf, string telefone, string endereco, string dataNasc, string email)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Telefone = telefone;
+            Endereco = endereco;
+            DataNasc = dataNasc;
+            Email = email;
+           
+        }
+
+        // impressão padrão
+        public override string ToString()
+        {
+
+            return "O QUARTO FOI ALUGADO POR:  "
+                + Nome.ToUpper()
+                + "\n * CPF: " + CPF.ToUpper()
+                + "\n * TELEFONE: "
+                + Telefone.ToUpper()
+                + "\n * ENDEREÇO: "
+                + Endereco.ToUpper()
+                + "\n * DATA DE NASCIMENTO: "
+                + DataNasc.ToUpper()
+                + "\n * EMAIL: "
+                + Email.ToUpper();
+             
+        }
+    }
+    // classe onde é declarado as variaveis
+    class Quartos
+    {
+        public float Periodo { get; set; }
+        public float ValorQuarto { get; set; }
+
+        public Quartos(float periodo, float valorQuarto)
+        {
+            Periodo = periodo;
+            ValorQuarto = valorQuarto;
+        }
+        // metodo para calcular o valor total
+        public double ValorTotal()
+        {
+            return Periodo * ValorQuarto;
+
+
+        }
+        // impressão padrão
+        public override string ToString()
+        {
+            return "\n * VALOR MENSAL: "
+                    + ValorQuarto.ToString("C")
+                    + "\n * VALOR A PAGAR: "
+                     + ValorTotal().ToString("C");
+        }
+    }
+ 
+}
+
---
+++ QuartoAluguel/QuartoAluguel/Program.cs
@@ -0,0 +1,110 @@
+﻿//using System;
+
+//namespace QuartoAluguel
+//{
+//    class Programa
+//    {
+
+//        static void Main(string[] args)
+//        {
+//            Cliente A = new Cliente();
+//            char Alugar;
+
+//            Console.WriteLine("******* RELATÓRIO DA HOSPEDAGEM *******");
+//            Console.WriteLine("NOME: ");
+//            A.Nome = Console.ReadLine();
+//            Console.WriteLine("CPF: ");
+//            A.CPF = Console.ReadLine();
+//            Console.WriteLine("TELEFONE: ");
+//            A.Telefone = Console.ReadLine();
+//            Console.WriteLine("ENDEREÇO: ");
+//            A.Endereco = Console.ReadLine();
+//            Console.WriteLine("DATA DE NASCIMENTO: ");
+//            A.DataNasc = Console.ReadLine();
+//            Console.WriteLine("EMAIL: ");
+//            A.email = Console.ReadLine();
+
+//            Console.WriteLine(); Console.WriteLine();
+//            Console.WriteLine("POR QUANTOS MESES DESEJA ALUGAR O QUARTO?: ");
+//            A.Periodo = float.Parse(Console.ReadLine());
+//            Console.WriteLine("QUAL O VALOR DO QUARTO POR MÊS: " + A.ValorQuarto);
+
+
+
+//            Console.WriteLine("DESEJA ALUGAR O QUARTO S/N: ");
+//            Alugar = char.Parse(Console.ReadLine());
+
+
+//            if(Alugar == 's' || Alugar == 'S')
+//            {
+//                Console.WriteLine(A);
+//            }
+//            else
+//            {
+//                Console.WriteLine(" SE PRECISAR DE ALGUMA COISA ESTAMOS A SUA DISPOSIÇÃO ");
+//            }
+
+
+//        }
+//    }
+//}
+
+using System;
+
+namespace QuartoAluguel
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE A QUANTIDADE DE QUARTOS DISPONIVEIS: ");// pede para você informar a quantidade de quartos
+            int qtd_quartos = int.Parse(Console.ReadLine());//guarda a quantidade de quartos
+            //armazena o tamanho do vetor em variavel
+            Cliente[] quarto = new Cliente[qtd_quartos];//declara a instancia o vetor do objeto "quarto" relacionado a classe "cliente"
+            Quartos[]quartos = new Quartos[qtd_quartos];//declara a instancia o vetor do objeto "quartos" relacionado a classe "Quartos"
+            
+
+            //inicia a "manipulação" ou navegação entre os indices do vetor 
+            //primeiro parametro:
+            //inicializa e atribui o valor ZERO (para acessar o indice ZERO - primeira linha )
+            for (int i = 0; i < qtd_quartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("*** DADOS DO HÓSPEDE " + (i + 1));
+                Console.WriteLine("NOME....: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("CPF: ");
+                string cpf = Console.ReadLine();
+                Console.WriteLine("TELEFONE: ");
+                string telefone = Console.ReadLine();
+                Console.WriteLine("ENDEREÇO: ");
+                string endereco = Console.ReadLine();
+                Console.WriteLine("DATA DE NASCIMENTO: ");
+                string dataNasc = Console.ReadLine();
+                Console.WriteLine("EMAIL: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("POR QUANTOS MESES DESEJA ALUGAR O QUARTO?: ");
+                float Periodo = float.Parse(Console.ReadLine());// armazena o tanto de mese que você digitou
+                Console.WriteLine("QUAL O VALOR DO QUARTO POR MÊS?: ");
+                float ValorQuarto = float.Parse(Console.ReadLine());// armazena o valor do quarto por mês
+                quartos[i] = new Quartos(Periodo, ValorQuarto);
+                quarto[i] = new Cliente(nome, cpf, telefone, endereco, dataNasc, email);
+
+            }
+            for (int i = 0; i < qtd_quartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("*** DADOS DO HÓSPEDE " + (i + 1));
+                Console.WriteLine(quarto[i]);
+                Console.WriteLine(quartos[i]);
+      
+
+                Console.WriteLine(); Console.WriteLine();
+               
+              
+
+            }
+        }
+    }
+}
