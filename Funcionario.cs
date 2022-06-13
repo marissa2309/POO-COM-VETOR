5e77c16f78ba117a270c4ae2c97f01ef283ae018
marissa2309 <102029727+marissa2309@users.noreply.github.com>
2022-06-13 08:07:20 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Dados_funcionario
+{
+    //classe onde está declarada as variaveis
+    class Dados
+    {
+        public string Nome { get; set; }
+        public double SalarioB { get; set; }
+        public double Imposto { get; set; }
+
+
+
+        public Dados(string nome, double salariob, double imposto)
+        {
+            Nome = nome;
+            SalarioB = salariob;
+            Imposto = imposto;
+        }
+        //método para calcular o salarioL
+        public double SalarioL()
+        {
+            return SalarioB - Imposto;
+        }
+        //método para mostrar o quanto será o aumento de salário
+        public void AumentarSlr(double aumento)
+        {
+            SalarioB = SalarioB + (SalarioB * aumento / 100);
+        }
+        //impressão padrão
+        public override string ToString()
+        {
+            return "Funcionário: "
+            + Nome.ToUpper()
+            + ",\n R$ "
+            + SalarioL().ToString("F2");
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,76 @@
+﻿//using System;
+
+//namespace Dados_funcionario
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            float aumento;
+//            Funcionario a = new Funcionario();
+
+//            Console.WriteLine("NOME: ");
+//            a.nome = Console.ReadLine();
+//            Console.WriteLine("SALÁRIO BRUTO: ");
+//            a.salarioBruto = double.Parse(Console.ReadLine());
+//            Console.WriteLine("QUAL O IMPOSTO: ");
+//            a.imposto = double.Parse(Console.ReadLine());
+
+//            Console.WriteLine("NOME: " + a.nome + " SALÁRIO BRUTO: " + a.salarioBruto + " SALÁRIO LIQUIDO: " + (a.salarioBruto - a.imposto));
+
+//            Console.WriteLine("QUANTO VAI SER SEU AUMENTO EM % SOBRE O SALÁRIO BRUTO: ");
+//            aumento = float.Parse(Console.ReadLine());
+
+//            aumento = aumento / 100;
+
+//            Console.WriteLine("O SEU SALÁRIO COM O AUMENTO É DE: " + (a.salarioBruto +(a.salarioBruto * aumento)));
+
+//            Console.WriteLine();
+//            Console.WriteLine("Pressione ENTER encerrar");
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+using System;
+
+namespace Dados_funcionario
+{
+    class Program
+    {
+       
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Quantos funcionários deseja digitar? ");//pergunta a quantidade de funcionarios
+                int qtd_f = int.Parse(Console.ReadLine());//guarda a quantidade de funcionarios
+
+
+                // armazena o tamanho do vetor em variavel
+                Dados[] dados = new Dados[qtd_f];
+
+
+
+                for (int i = 0; i < qtd_f; i++)
+                {
+                    Console.WriteLine("NOME: ");//pergunta o nome 
+                    String nome = Console.ReadLine();//guarda o nome digitado
+                    Console.WriteLine("SALÁRIO BRUTO: ");//pergunta o salario bruto
+                    double salariob = double.Parse(Console.ReadLine());//guarda o salario bruto
+                    Console.WriteLine("IMPOSTO: ");
+                    double imposto = double.Parse(Console.ReadLine());
+
+
+
+                    dados[i] = new Dados(nome, salariob, imposto);
+
+                    Console.WriteLine(dados[i]);//mostra os dados
+                    Console.WriteLine();
+                    Console.WriteLine("Quanto vai ser o aumento em % sobre o salário bruto: ");
+                    dados[i].AumentarSlr(double.Parse(Console.ReadLine()));
+                    Console.WriteLine(dados[i]);
+                }
+            }
+    }
+}
